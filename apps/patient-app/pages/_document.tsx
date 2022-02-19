3140c352fb3c35131836db0d32e6fd36937d0567
Amirhossein Beigi <dev.amirhbeigi@gmail.com>
2022-02-19 10:15:09 +0330

    refactor(patientApp): in document file class component to function component

--- apps/patient-app/pages/_document.tsx
+++ apps/patient-app/pages/_document.tsx
@@ -1,18 +1,15 @@
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
-class Document extends NextDocument {
-    static async getInitialProps(ctx: any) {
-        return await NextDocument.getInitialProps(ctx);
-    }
-    render() {
-        return (
-            <Html dir="rtl" lang="fa-IR" className="scroll-smooth">
-                <Head />
-                <body className="bg-[#f3f7fa]">
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        );
-    }
-}
-export default Document;
+import type { NextComponentType } from 'next';
+import { Head, Html, Main, NextScript } from 'next/document';
+
+const CustomDocument: NextComponentType = () => {
+    return (
+        <Html dir="rtl" lang="fa-IR" className="scroll-smooth">
+            <Head />
+            <body className="bg-[#f3f7fa]">
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
+};
+export default CustomDocument;
