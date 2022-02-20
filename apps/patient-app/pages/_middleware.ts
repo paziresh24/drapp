08052aff4d00673735e3d1b6fee29df81040ae85
Amirhossein Beigi <dev.amirhbeigi@gmail.com>
2022-02-20 18:17:21 +0330

    refactor(patient-app): change name P24SESSION to p24Session and define siginPageRedirectUrl variable

--- apps/patient-app/pages/_middleware.ts
+++ apps/patient-app/pages/_middleware.ts
@@ -2,11 +2,11 @@ import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
 export function middleware(req: NextRequest) {
-    const { certificate, P24SESSION } = req.cookies;
+    const { certificate, P24SESSION: p24Session } = req.cookies;
 
-    if (!certificate || !P24SESSION) {
-        return NextResponse.redirect(
-            `${process.env.NEXT_PUBLIC_CLINIC_BASE_URL}/signin/?url=${req.url}`
-        );
+    const siginPageRedirectUrl = `${process.env.NEXT_PUBLIC_CLINIC_BASE_URL}/signin/?url=${req.url}`;
+
+    if (!certificate || !p24Session) {
+        return NextResponse.redirect(siginPageRedirectUrl);
     }
 }
