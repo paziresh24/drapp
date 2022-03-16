030eb024ddc079bd3d5e282fa094d35d48f741ab
Sobhan Sharafi <sobhan0225@gmail.com>
2022-03-16 12:26:01 +0330

    feat: add new tests for other modes
    add test for Salamat-Unauthorized error and create center duplicated error

--- apps/drapp-e2e/src/integration/salamatLogin.spec.ts
+++ apps/drapp-e2e/src/integration/salamatLogin.spec.ts
@@ -14,12 +14,28 @@ describe('drapp', () => {
         });
     });
 
+    it('should fail salamat login and return "Salamat-Unauthorized" error', async () => {
+        cy.getByTestId('change-login-with-salamat').click();
+        cy.getByTestId('salamat-login-username-field').type('testUsername');
+        cy.getByTestId('salamat-login-password-field').type('testPass');
+        cy.getByTestId('salamat-login-login-button').click();
+        cy.intercept('POST', '/V1/salamat-auth/login', {
+            fixture: 'salamatUnauthorized',
+            statusCode: 401
+        }).as('login');
+        cy.wait('@login').then(xhr => {
+            console.log(xhr);
+            expect(window.localStorage.getItem('token')).to.eq(null);
+            expect(xhr.response.body.message).to.eq('Salamat-Unauthorized');
+        });
+    });
+
     it('should successful create center then login with salamat', async () => {
         cy.intercept('POST', '/V1/doctor/center/ignoreShahkar', {
             fixture: 'createCenterSuccess'
         }).as('createCenter');
-        let interceptCount = 0;
 
+        let interceptCount = 0;
         cy.intercept('POST', '/V1/salamat-auth/login', req => {
             req.reply(res => {
                 if (interceptCount === 0) {
@@ -46,6 +62,40 @@ describe('drapp', () => {
             cy.wait('@login').then(() => {
                 expect(window.localStorage.getItem('token')).to.eq('faketoken');
             });
+        });
+    });
+
+    it('should fail create center and return error: duplicated', async () => {
+        cy.intercept('POST', '/V1/doctor/center/ignoreShahkar', {
+            fixture: 'fieldCreateCenter',
+            statusCode: 401
+        }).as('createCenter');
+
+        let interceptCount = 0;
+        cy.intercept('POST', '/V1/salamat-auth/login', req => {
+            req.reply(res => {
+                if (interceptCount === 0) {
+                    console.log(interceptCount);
+                    res.send({ fixture: 'salamatUnAuthenticatedUser', statusCode: 404 });
+                    interceptCount = 1;
+                } else {
+                    res.send({ fixture: 'salamatAuthenticatedUser', statusCode: 200 });
+                }
+            });
+        }).as('login');
+        cy.getByTestId('change-login-with-salamat').click();
+        cy.getByTestId('salamat-login-username-field').type('testUsername');
+        cy.getByTestId('salamat-login-password-field').type('testPass');
+        cy.getByTestId('salamat-login-login-button').click();
+
+        cy.wait('@login');
+        cy.getByTestId('create-center-cell-phone-field').type('09122222222');
+        cy.getByTestId('create-center-national-code-field').type('1111111111');
+        cy.getByTestId('create-center-submit-button').click();
+
+        cy.wait('@createCenter').then(xhr => {
+            expect(window.localStorage.getItem('token')).to.eq(null);
+            expect(xhr.response.body.message).to.eq('duplicated');
         });
     });
 });
