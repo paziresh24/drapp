80263e2069d548a0e2db1b4a5bfd489a34ef73ae
Amirhossein <dev.amirhbeigi@gmail.com>
2022-07-12 12:38:22 +0430

    feat(drapp): change link seting in super menu

--- libs/configs/drapp/menu.config.js
+++ libs/configs/drapp/menu.config.js
@@ -5,19 +5,17 @@ import {
     LearnIcon,
     ComplaintsIcon,
     PrescriptionMenuIcon,
-    SettingIcon
+    PrescriptionIcon
 } from '@paziresh24/shared/icon';
 
 import { StarIcon } from '@paziresh24/shared/icon/public/duotone';
 import { useGetFeedbacks } from '@paziresh24/hooks/drapp/profile';
 import { useDrApp } from '@paziresh24/context/drapp';
 import { useSupport } from '@paziresh24/context/core/supportChat';
-import { useSettingTurns } from 'apps/drapp/src/components/molecules/turning/statusBar/settingTurns.context';
 
 export const MainMenuData = () => {
     const [info] = useDrApp();
     const getFeedbacks = useGetFeedbacks({ center_id: info.center.id });
-    const [, setIsOpen] = useSettingTurns();
 
     const calculateNoReplyComments = () => {
         if (!getFeedbacks.data.result) return 0;
@@ -30,49 +28,49 @@ export const MainMenuData = () => {
         {
             title: 'نسخه های ثبت شده',
             path: '/prescription',
-            icon: <PrescriptionMenuIcon color="#3F3F79" />
+            icon: <PrescriptionMenuIcon color="#000" />
+        },
+        {
+            title: 'بیمه های من',
+            path: '/providers',
+            icon: <PrescriptionIcon color="#000" />
         },
         {
             title: 'پراستفاده ها',
             path: '/favorite/templates',
-            icon: <StarIcon color="#3F3F79" />
+            icon: <StarIcon color="#000" />
         },
         {
             title: 'مشاوره',
             path: '/consult',
-            icon: <ConsultIcon color="#3F3F79" />
+            icon: <ConsultIcon color="#000" />
         },
         {
             title: 'تسویه حساب',
             path: '/financial',
-            icon: <CardIcon color="#3F3F79" />
-        },
-        {
-            title: 'تنظیمات نوبت دهی',
-            onClick: () => setIsOpen(true),
-            icon: <SettingIcon color="#3F3F79" />
+            icon: <CardIcon color="#000" />
         },
         {
             title: 'آموزش',
             path: '/learn',
-            icon: <LearnIcon color="#3F3F79" />
+            icon: <LearnIcon color="#000" />
         },
         {
             title: 'نظرات بیماران',
             path: '/feedbacks',
-            icon: <MessageIcon color="#3F3F79" />,
+            icon: <MessageIcon color="#000" />,
             badge: getFeedbacks.isSuccess && calculateNoReplyComments()
         },
         {
             title: 'شکایات',
             path: '/complaints',
-            icon: <ComplaintsIcon color="#3F3F79" />
+            icon: <ComplaintsIcon color="#000" />
         }
     ];
 };
 
 export const SubMenuData = () => {
-    const [supportModal, setSupportModal] = useSupport();
+    const [, setSupportModal] = useSupport();
 
     return [
         {
