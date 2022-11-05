5baf40a67dbedbda8276fa01836f21c64eb6ef68
Amirhossein <dev.amirhbeigi@gmail.com>
2022-11-05 10:06:55 +0330

    fix(prescription): zero fixed count input

--- apps/prescription/src/components/molecules/details/atom/Count.jsx
+++ apps/prescription/src/components/molecules/details/atom/Count.jsx
@@ -24,15 +24,22 @@ const Count = ({
         focus ? ref.current.focus() : ref.current.blur();
     }, [focus]);
 
+    const handleOnBlur = () => {
+        setFocus && setFocus(false);
+        if (ref.current.value < 1) {
+            ref.current.value = 1;
+            onChange(1);
+        }
+    };
+
     return (
         <TextField
             ref={ref}
             error={error}
             label={label ?? 'تعداد'}
             type="number"
-            min="1"
-            onBlur={() => setFocus && setFocus(false)}
-            min={min}
+            onBlur={handleOnBlur}
+            min={min ?? '1'}
             max={max}
             className={`${!autoWidth && styles.countInput} ${simple ? styles.simple : ''}`}
             onChange={e => onChange(e.target.value ?? null)}
