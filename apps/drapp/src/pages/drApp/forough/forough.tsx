5fc7fb78f1d48868e612de3de90922f017821fe1
Amirhossein <dev.amirhbeigi@gmail.com>
2023-01-29 10:36:28 +0330

    feat(drapp): add wating time in fourogh page

--- apps/drapp/src/pages/drApp/forough/forough.tsx
+++ apps/drapp/src/pages/drApp/forough/forough.tsx
@@ -2,8 +2,10 @@ import { Button, Skeleton } from '@mui/material';
 import { getSplunkInstance } from '@paziresh24/shared/ui/provider';
 import { useSearchViewInfo } from 'apps/drapp/src/apis/forough/searchViewInfo';
 import { round } from 'lodash';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import GaugeChart from 'react-gauge-chart';
+import { InfoIcon } from '@paziresh24/shared/icon';
 
 export const Forough = () => {
     const searchViewInfo = useSearchViewInfo();
@@ -12,15 +14,25 @@ export const Forough = () => {
     const router = useHistory();
 
     useEffect(() => {
-        if (searchViewInfo.data?.data) {
-            setIsLoading(false);
-            setData(searchViewInfo.data?.data);
-        } else {
-            setTimeout(() => {
-                searchViewInfo.refetch();
-            }, 5000);
+        if (searchViewInfo.isSuccess) {
+            if (searchViewInfo.data?.data) {
+                setIsLoading(false);
+                setData(searchViewInfo.data?.data);
+            } else {
+                setTimeout(() => {
+                    searchViewInfo.refetch();
+                }, 5000);
+            }
         }
-    }, [searchViewInfo.data]);
+    }, [searchViewInfo.status, searchViewInfo.data]);
+
+    const waitingTimeText = useMemo(() => {
+        const watingTimeAvarageTime = data?.waiting_time_info?.waiting_time ?? 0;
+        if (watingTimeAvarageTime < 0.5) return 'حداکثر نیم ساعت';
+        if (watingTimeAvarageTime >= 0.5 && watingTimeAvarageTime < 1) return 'نیم تا 1 ساعت';
+        if (watingTimeAvarageTime >= 1 && watingTimeAvarageTime < 2) return '1 تا 2 ساعت';
+        if (watingTimeAvarageTime >= 2) return 'بیشتر از 2 ساعت';
+    }, [data?.waiting_time_info?.waiting_time]);
 
     return (
         <div className="flex flex-col max-w-screen-sm p-5 mx-auto space-y-5">
@@ -31,6 +43,64 @@ export const Forough = () => {
                 <Skeleton width="100%" height="16rem" className="!scale-100" />
             ) : (
                 <div className="flex flex-col space-y-2">
+                    <div className="flex flex-col p-3 space-y-3 bg-white border border-solid rounded-lg border-slate-200">
+                        <span className="text-sm font-medium leading-6">
+                            میانگین زمان انتظار بیماران در مرکز درمانی شما
+                        </span>
+                        <div className="flex items-center justify-between space-s-2">
+                            <ul className="space-y-2 text-xs text-slate-500">
+                                <li className="before:content-['\2022'] before:text-[#8FBB13] before:font-bold before:text-4xl before:absolute relative before:-top-[0.5rem] before:right-0 pr-4">
+                                    حداکثر نیم ساعت
+                                </li>
+                                <li className="before:content-['\2022'] before:text-[#F1DB18] before:font-bold before:text-4xl before:absolute relative before:-top-[0.5rem] before:right-0 pr-4">
+                                    نیم تا 1 ساعت
+                                </li>
+                                <li className="before:content-['\2022'] before:text-[#FF8E01] before:font-bold before:text-4xl before:absolute relative before:-top-[0.5rem] before:right-0 pr-4">
+                                    1 تا 2 ساعت
+                                </li>
+                                <li className="before:content-['\2022'] before:text-[#E82929] before:font-bold before:text-4xl before:absolute relative before:-top-[0.5rem] before:right-0 pr-4">
+                                    بیشتر از 2 ساعت
+                                </li>
+                            </ul>
+                            <div className="flex flex-col">
+                                <GaugeChart
+                                    animate={false}
+                                    arcWidth={0.2}
+                                    className="!w-56 !-ml-5"
+                                    nrOfLevels={4}
+                                    cornerRadius={3}
+                                    arcPadding={0.03}
+                                    percent={((data?.waiting_time_info?.waiting_time ?? 0) * 2) / 4}
+                                    hideText={true}
+                                    colors={['#8FBB13', '#F1DB18', '#FF8E01', '#E82929']}
+                                />
+                                <div className="py-2 mr-4 text-xs text-center rounded-md bg-slate-100">
+                                    {waitingTimeText}
+                                </div>
+                            </div>
+                        </div>
+                        <div className="flex items-center p-2 rounded-md space-s-2 bg-slate-100">
+                            <InfoIcon color="#0070f3" width={18} />
+                            <span className="text-xs font-medium">
+                                {(data?.waiting_time_info?.waiting_time ?? 0) < 0.5 ? (
+                                    <>
+                                        <span className="font-semibold text-secondary">
+                                            زمان انتظار بیمار در مطب شما، در بازه خیلی خوب قرار
+                                            دارد.
+                                        </span>
+                                    </>
+                                ) : (
+                                    <>
+                                        شما برای بهبود رتبه خود، نیاز به{' '}
+                                        <span className="font-semibold text-secondary">
+                                            کاهش زمان انتظار
+                                        </span>{' '}
+                                        دارید.
+                                    </>
+                                )}
+                            </span>
+                        </div>
+                    </div>
                     <div className="flex items-center justify-between p-3 bg-white border border-solid rounded-lg border-slate-200 space-s-2">
                         <span className="text-sm font-medium leading-6">
                             تعداد بیمارانی که از پروفایل شما در پذیرش24 بازدید کردند.
