cd617dbcfa2e11103e5b17c84a39a4ea4218ff68
amirhosseinmousavi <a.hossainmousavi@gmail.com>
2023-04-16 12:30:03 +0430

    chore: change doctor onboarding  video link

--- apps/drapp/src/pages/drApp/activation/consult/onboarding.tsx
+++ apps/drapp/src/pages/drApp/activation/consult/onboarding.tsx
@@ -6,7 +6,7 @@ import { useHistory } from 'react-router-dom';
 export const Onboarding = () => {
     const router = useHistory();
     const videoIfram =
-        '<style>.h_iframe-aparat_embed_frame{position:relative;}.h_iframe-aparat_embed_frame .ratio{display:block;width:100%;height:auto;}.h_iframe-aparat_embed_frame iframe{position:absolute;top:0;left:0;width:100%;height:100%;}</style><div class="h_iframe-aparat_embed_frame"><span style="display: block;padding-top: 57%"></span><iframe src="https://www.aparat.com/video/video/embed/videohash/prTM9/vt/frame"  allowFullScreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe></div>';
+        '<style>.h_iframe-aparat_embed_frame{position:relative;}.h_iframe-aparat_embed_frame .ratio{display:block;width:100%;height:auto;}.h_iframe-aparat_embed_frame iframe{position:absolute;top:0;left:0;width:100%;height:100%;}</style><div class="h_iframe-aparat_embed_frame"><span style="display: block;padding-top: 57%"></span><iframe src="https://www.aparat.com/video/video/embed/videohash/s9xqF/vt/frame?recom=self"  allowFullScreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe></div>';
 
     const handleContinueButton = () => {
         getSplunkInstance().sendEvent({
