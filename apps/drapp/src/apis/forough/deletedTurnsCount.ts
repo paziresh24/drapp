0b9d93fa3591e048644f2cedb41b62da0803e695
amirshahriarmousavi <a.hosainmousavi@gmail.com>
2023-12-21 13:04:50 +0330

    chore: change deleted api params and change if for show data in forough

--- apps/drapp/src/apis/forough/deletedTurnsCount.ts
+++ apps/drapp/src/apis/forough/deletedTurnsCount.ts
@@ -3,17 +3,17 @@ import { apiGatewayClient } from '../apiGatewayClient';
 
 interface Params {
     user_center_id: string;
-    from_less_than?:number | string;
-    from_greather_than?:number | string;
+    from_less_than?: number | string;
+    from_greather_than?: number | string;
     payment_status_in?: number;
 }
 
-const getDeletedTurnsCount = async ({ user_center_id, from_greather_than,from_less_than,payment_status_in }: Params) => {
-    return await apiGatewayClient.get(
-        `/v1/appointments/count?user_center_id=${user_center_id}&from_less_than=${from_less_than}&from_greather_than=${from_greather_than}&payment_status_in=${payment_status_in}&deleted_at_greater_than=from`
-    );
+const getDeletedTurnsCount = async (params: Params) => {
+    return await apiGatewayClient.get(`/v1/appointments/count`, { params });
 };
 
 export const useGetDeletedTurnsCount = (params: Params, option?: Record<string, any>) => {
-    return useQuery(['getDeletedTurnsCount', params], () => getDeletedTurnsCount(params), { ...option });
+    return useQuery(['getDeletedTurnsCount', params], () => getDeletedTurnsCount(params), {
+        ...option
+    });
 };
