bd7db38b3c3d326f09b322cb2604b350491e067f
Amirhossein <dev.amirhbeigi@gmail.com>
2024-03-11 13:22:43 +0330

    feat: add fragments

---
+++ apps/drapp/src/fragment/components.tsx
@@ -0,0 +1,13 @@
+// import {
+//     PlasmicInAppNotification,
+//     PlasmicInAppNotification__ArgProps
+// } from '.plasmic/plasmic/paziresh_24_notification/PlasmicInAppNotification';
+
+export const Components = {
+    InAppNotification: {
+        // Component: PlasmicInAppNotification,
+        id: 'TvLiAjgrI0Pc',
+        // propsAllowed: PlasmicInAppNotification__ArgProps,
+        projectId: 'baKTW3UAa7cxh71yA6NuMk'
+    }
+};
