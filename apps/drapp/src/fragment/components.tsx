254e1f86d770ed4cefdde229e203bcfc206eef6c
Amirhossein <dev.amirhbeigi@gmail.com>
2024-04-23 03:47:11 +0330

    chore: add fragment popover component

--- apps/drapp/src/fragment/components.tsx
+++ apps/drapp/src/fragment/components.tsx
@@ -3,11 +3,15 @@
 //     PlasmicInAppNotification__ArgProps
 // } from '.plasmic/plasmic/paziresh_24_notification/PlasmicInAppNotification';
 
+import PlasmicHoursDaysOfWeek, {
+    PlasmicHoursDaysOfWeek__ArgProps
+} from '../../../../.plasmic/plasmic/opium/PlasmicHoursDaysOfWeek';
+
 export const Components = {
-    InAppNotification: {
-        // Component: PlasmicInAppNotification,
-        id: 'TvLiAjgrI0Pc',
-        // propsAllowed: PlasmicInAppNotification__ArgProps,
-        projectId: 'baKTW3UAa7cxh71yA6NuMk'
+    HoursDaysOfWeek: {
+        Component: PlasmicHoursDaysOfWeek,
+        id: 'V1gAPdcbOPOO',
+        propsAllowed: PlasmicHoursDaysOfWeek__ArgProps,
+        projectId: 'rqmTJ9WSF4RQaQFCvo2iUX'
     }
 };
